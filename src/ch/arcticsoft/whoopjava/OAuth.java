775637f16206aa37fac07ef7839adca9225bdb2f
ante <ante@leopard.local>
2025-10-12 17:08:21 +0200

    i

---
+++ src/ch/arcticsoft/whoopjava/OAuth.java
@@ -0,0 +1,12 @@
+package ch.arcticsoft.whoopjava;
+
+import java.net.http.*;
+import java.net.URI;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.time.Duration;
+import com.fasterxml.jackson.databind.*;
+
+public class OAuth {
+
+}
