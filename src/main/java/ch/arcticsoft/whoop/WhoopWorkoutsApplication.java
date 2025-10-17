f526718e607b24507508edcca8939aa22fd2b55d
ante <ante@leopard.local>
2025-10-17 21:02:53 +0200

    in

---
+++ src/main/java/ch/arcticsoft/whoop/WhoopWorkoutsApplication.java
@@ -0,0 +1,13 @@
+package ch.arcticsoft.whoop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhoopWorkoutsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhoopWorkoutsApplication.class, args);
+	}
+
+}
